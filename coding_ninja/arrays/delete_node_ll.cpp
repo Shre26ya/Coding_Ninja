09156545f95013624b94bf7bc7a669b9698f37fc
Shre26ya <shreyasudhakaran5@gmail.com>
2024-03-03 13:57:58 -0500

    Prac

---
+++ coding_ninja/arrays/delete_node_ll.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class Node {
+ * public:
+ *     int data;
+ *     Node *next;
+ *     Node() : data(0), next(nullptr) {}
+ *     Node(int x) : data(x), next(nullptr) {}
+ *     Node(int x, Node *next) : data(x), next(next) {}
+ * };
+ */
+
+Node *deleteLast(Node *head){
+    if(head == NULL || head->next == NULL) return NULL;
+
+    Node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+
+    delete temp->next;
+    temp->next = nullptr;
+
+    return head;
+
+
+
+}
